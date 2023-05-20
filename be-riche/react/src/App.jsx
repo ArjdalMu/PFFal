f223477273a4a6d477277a70ae20a132e5ab48d9
HichamEh <94751518+HichamEh@users.noreply.github.com>
2023-05-20 16:01:06 +0000

    Update App.jsx

--- be-riche/react/src/App.jsx
+++ be-riche/react/src/App.jsx
@@ -11,6 +11,7 @@ export default function App() {
       <Route path="/" element={<Home/>} />
       <Route path="/login" element={<Login/>} />
     </Routes>
+<Footer />
     </BrowserRouter>
   )
 }
